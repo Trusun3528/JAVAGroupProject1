60c9b76a590a8973d926f3d08e24f5adfb7d958d
Austin Cameron <115508747+Trusun3528@users.noreply.github.com>
2025-03-07 15:37:25 -0500

    Did part 2

---
+++ src/main/java/util/DBUtil.java
@@ -0,0 +1,31 @@
+package util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBUtil {
+
+	public static Connection getConnection() {
+	    Connection connection = null;
+	    try {
+	        String url = "jdbc:mysql://192.168.87.24:3306/librarydb"; // change for you
+	        String username = "root"; // change for you
+	        String password = "Password1"; // change for you
+	        connection = DriverManager.getConnection(url, username, password);
+	    } catch (SQLException e) {
+	        e.printStackTrace();
+	    }
+	    return connection;
+	}
+	
+	public static void main(String[] args) {
+	    Connection conn = DBUtil.getConnection();
+	    if (conn != null) {
+	        System.out.println("Connection established successfully!");
+	    } else {
+	        System.out.println("Failed to establish connection.");
+	    }
+	}
+}
+	
